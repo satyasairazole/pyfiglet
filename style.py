9b6f1019a7cd5fd1f3f0da06b3b816464be298cb
satysairazole <srazole@gmail.com>
2021-09-20 10:17:27 +0530

    pyfiglet files are added

---
+++ style.py
@@ -0,0 +1,26 @@
+# import pyfiglet module
+import pyfiglet
+#Text in default font
+a = pyfiglet.figlet_format("satyasairazole")
+print(a)
+#Text in slant font
+b= pyfiglet.figlet_format("satyasairazole", font="slant")
+print(b)
+#Text in 3D font
+c = pyfiglet.figlet_format("satyasairazole", font="3-d")
+print(c)
+#Text in 3×5 font
+d= pyfiglet.figlet_format("satyasairazole", font="3x5")
+print(d)
+#Text in alligator font
+e = pyfiglet.figlet_format("satyasairazole", font="alligator")
+print(e)
+#Text in dot matrix font
+f= pyfiglet.figlet_format("satyasairazole", font="dotmatrix")
+print(f)
+#Text in bubble font
+g= pyfiglet.figlet_format("satyasairazole", font="bubble")
+print(g)
+# Text in digital font
+h= pyfiglet.figlet_format("satyasairazole", font = "digital" )
+print(h)
